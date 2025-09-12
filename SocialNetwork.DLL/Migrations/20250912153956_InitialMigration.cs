042313a6eb4f9e1d4977d2537b43074d746d4ae8
Valeriia <valeriia.vp1@yandex.ru>
2025-09-12 18:48:12 +0300

    Added migrations files

---
+++ SocialNetwork.DLL/Migrations/20250912153956_InitialMigration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace SocialNetwork.DLL.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+          
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            
+        }
+    }
+}
